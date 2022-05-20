f4478fa5a71ad51b85f4385de95f7d487a28cdff
Jeff McCrory <jmccrory@nanthealth.com>
2022-05-20 10:42:36 +0100

    Update Annotation.cs

--- Models/Annotation.cs
+++ Models/Annotation.cs
@@ -8,8 +8,8 @@
 
     public class Box
     {
-        public int Left { get; set; } = 0;
-        public int Top { get; set; } = 0;
+        public int Y { get; set; } = 0;
+        public int X { get; set; } = 0;
         public int Width { get; set; } = 0;
         public int Height { get; set; } = 0;
 
